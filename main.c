4162349c94a1d992be2f8a4f60259286c73aebd6
hamana-lab <hamana@sookmyung.ac.kr>
2025-10-02 15:21:10 +0900

    week05_2

--- main.c
+++ main.c
@@ -7,12 +7,10 @@ int main(int argc, char *argv[])
   printf("Input an integer:");
   scanf("%d", &num);
   
-  if (num>0)
-   printf("positive number\n");
-  else if (num==0)
-   printf("It's 0.\n");
+  if (num>=0)
+   printf("Absolute value is %d\n", num);
   else
-   printf("negative number\n");
+   printf("Absolute value is %d\n", -num);
     
   system("PAUSE");	
   return 0;
